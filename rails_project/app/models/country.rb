c13d702d84fddc3a496f67287ce368f921341239
prog-allies <prog.allies@gmail.com>
2017-05-06 19:33:09 +0000

    Add EU Vote by Country data.

--- rails_project/app/models/country.rb
+++ rails_project/app/models/country.rb
@@ -3,4 +3,6 @@ class Country < ApplicationRecord
   has_many :constituencies, through: :regions
 
   has_many :population_estimates, -> { order(estimate_date: :desc) },  as: :area, class_name: "AreaPopulationEstimate"
+
+  has_one :eu_vote, as: :area, class_name: "AreaEuVote"
 end
---
+++ rails_project/db/seeds/09_country_euref_vote.rb
@@ -0,0 +1,7 @@
+# Origianl data from:
+# http://www.bbc.co.uk/news/politics/eu_referendum/results
+
+AreaEuVote.create!(area: Country.find('England'), actual_leave_votes: 15188406, actual_remain_votes: 13266996, actual_leave_vote_percent: 53.3761779222, contains_postal_votes: false)
+AreaEuVote.create!(area: Country.find('Wales'), actual_leave_votes: 854572, actual_remain_votes: 772347, actual_leave_vote_percent: 52.5270157887, contains_postal_votes: false)
+AreaEuVote.create!(area: Country.find('Scotland'), actual_leave_votes: 1018322, actual_remain_votes:  1661191, actual_leave_vote_percent: 38.0039955022, contains_postal_votes: false)
+AreaEuVote.create!(area: Country.find('Northern Ireland'), actual_leave_votes: 349442, actual_remain_votes: 440707, actual_leave_vote_percent: 44.2248234194, contains_postal_votes: false)
